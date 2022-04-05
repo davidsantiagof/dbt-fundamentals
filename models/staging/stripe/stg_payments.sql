5ebf31caf2e2eac35fff890a40ed7cfb44fcbb15
david.flechas@factored.ai <david.flechas@factored.ai>
2022-04-05 03:12:17 +0000

    dbt-jinja-macros-packages: macros

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,7 +3,10 @@ select
         ,orderid as order_id
         ,paymentmethod as payment_method
         ,status
-        ,amount/100 as amount
+        --,amount/100 as amount
+        ,{{cents_to_dollars('amount',4)}} as amount
         ,created as created_at 
         
 from    {{ source('stripe', 'payment') }}--raw.stripe.payment
+
+{{limit_data_in_dev('created',5)}}
